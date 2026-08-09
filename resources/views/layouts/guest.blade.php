36ba52ac6f98f2a0e86a80c32cf50984fbf25cd1
Emmi73 <164627994+Emmi73@users.noreply.github.com.>
2026-08-09 01:33:09 +0100

    new7

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,6 +13,7 @@
 
         <!-- Scripts -->
         @vite(['resources/js/app.js'])
+        <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
