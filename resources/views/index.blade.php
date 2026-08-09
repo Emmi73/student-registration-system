96fac841a4c30b20461e02dda246986b3746af67
Emmi73 <164627994+Emmi73@users.noreply.github.com.>
2026-08-09 01:10:09 +0100

    new4

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Student Registration System</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
     
 </head>
 
